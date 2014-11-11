a3d7671d6bd076fb43b55c3251f009f082c3c6e3
Amoki <hugoduroux@gmail.com>
2014-11-11 10:50:37 +0100

    draft test for cleanUp

--- test/retrieve.js
+++ test/retrieve.js
@@ -3,4 +3,26 @@
 require('should');
 var config = require('../config/configuration.js');
 
+var cleanUp = require('../lib/helpers/retrieve.js').cleanUp;
+var card = require('./card.json');
+
 // Check data are retrieved from PROVIDER
+
+
+describe('cleanUp', function() {
+  it('should clean cards objects', function(done) {
+    var cleanCard = cleanUp(card);
+    cleanCard.should.have.properties([
+      'title',
+      'dateLastActivity',
+      'labels',
+      'actions',
+      'checklists',
+      'url',
+      'title',
+      'description'
+    ]);
+
+    done();
+  });
+});
